a1a8b2bfde2b1068f14579fdb01e5ef1751ec52d
Isaac Gateño <isaac@gateno.net>
2012-05-22 21:34:37 -0700

    added agency form

--- js/views/forms.js
+++ js/views/forms.js
@@ -39,6 +39,47 @@ var CountryFormView = Backbone.View.extend({
 
 });
 
+var AgencyFormView = Backbone.View.extend({
+
+  initialize: function() {
+    this.template = _.template($('#new-agency-template').html());
+  },
+
+  events: {
+    'click #add-agency': 'addAgency',
+    'keypress #new-agency #name': 'agencyEnter'
+  },
+
+  render: function() {
+    $(this.el).html(this.template());
+    return this;
+  },
+
+  addAgency: function(e) {
+    e.preventDefault();
+    var self = this;
+    this.model.set({
+      name: $('#new-agency #name').val(),
+      type: 2
+    });
+    this.model.save(null, {
+      success: function(model) {
+        self.model = new Entity();
+        self.render();
+        $('#feedback').html('Success!');
+      },
+      error: function() {
+        $('#feedback').html('error');
+      }
+    });
+  },
+
+  agencyEnter: function(e) {
+    if (e.keyCode == 13) this.addAgency(e);
+  }
+
+});
+
 var FormsView = Backbone.View.extend({
 
   initialize: function() {
@@ -56,34 +97,11 @@ var FormsView = Backbone.View.extend({
       this.countryFormView = new CountryFormView({model:country});
     }
     $('#new-country-container').html(this.countryFormView.render().el);
+    if (!this.agencyFormView) {
+      var agency = new Entity();
+      this.agencyFormView = new AgencyFormView({model:agency});
+    }
+    $('#new-agency-container').html(this.agencyFormView.render().el);
   },
 
 });
-
-/*var FormsView = Backbone.View.extend({
-
-  initialize: function() {
-    this.template = _.template(tpl.get('forms'));
-  },
-
-  events: {
-    "click #feedback a": "tabs",
-    "click #save_new_country": "saveNewCountry"
-  },
-
-  render: function() {
-    $(this.el).html(this.template());
-    return this;
-  },
-
-  tabs: function(e) {
-    e.preventDefault();
-    $(this).tab('show');
-  },
-
-  saveNewCountry: function(e) {
-    e.preventDefault();
-    console.log("Saving new country...");
-  }
-
-});*/
