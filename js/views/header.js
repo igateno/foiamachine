0cb09e0d8f8e75ac8a16edd415e3817cda2a0b9c
Isaac Gateño <isaac@gateno.net>
2012-05-23 21:15:11 -0700

    added navigation links to the topbar

--- js/views/header.js
+++ js/views/header.js
@@ -4,9 +4,27 @@ var HeaderView = Backbone.View.extend({
     this.template = _.template(tpl.get('header'));
   },
 
+  events: {
+    'click .navbar a.dash':'dash',
+    'click .navbar a.request':'request',
+    'click .navbar a.forms':'forms'
+  },
+
   render: function() {
     $(this.el).html(this.template());
     return this;
+  },
+
+  dash: function() {
+    app.navigate('dash', {trigger: true});
+  },
+
+  request: function() {
+    app.navigate('request', {trigger: true});
+  },
+
+  forms: function() {
+    app.navigate('forms', {trigger: true});
   }
 
 });
