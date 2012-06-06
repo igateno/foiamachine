88687db3904873147cb138fba61a41a2200e3160
krsnapaudel <krsnapaudel@hotmail.com>
2012-06-06 12:35:43 -0700

    corncobs added

--- api/mailer.php
+++ api/mailer.php
@@ -6,15 +6,28 @@
         ezcBase::autoload( $className );
     }
 
-    function sendMail(){
+    function sendMail(){    
        $mail = new ezcMailComposer();
-       $mail->from = new ezcMailAddress( 'dummy.user@foiamachine.org', 'Dummy User' );
-       $mail->addTo( new ezcMailAddress( 'requestengine@foiamachine.org', 'Request Engine' ) );
+       $mail->from = new ezcMailAddress( 'requestengine@foiamachine.org', 'FOIA Machine Request Engine' );
+       $mail->addTo( new ezcMailAddress( 'dummy.agency@foiamachine.org', 'Dummy Agency' ) );
+       //$mail->addCc( new ezcMailAddress( 'dummy.user@foiamachine.org', 'Dummy User' ) );
        $mail-> subject = "Hello foia, this is the subject";
        $mail->plainText = "I have been trying to reach for more than a week";
        $mail->build();
        $transport = new ezcMailMtaTransport();
        $transport->send($mail);
     }
+
+    function getConnection(){
+    	$dbhost = "mysql.foiamachine.org";
+     	$dbuser = "foia";
+     	$dbpassphrase = "foiamachine";
+     	$dbname = "foiastagingdb";
+     	$dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser,
+          $dbpassphrase);
+     	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     	return $dbh;
+    }
+
     sendMail(); 
 ?>
